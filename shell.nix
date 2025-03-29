b72bbd5589c2ad5df59eeeb2a4c401fc3d21920e
sswastik02 <sswastik02@gmail.com>
2025-03-29 20:03:56 +0530

    Added shell applications for create_disk and start_vmw

--- shell.nix
+++ shell.nix
@@ -9,6 +9,47 @@ mkShell {
     bison
     elfutils
     openssl
+    zlib
+    glibc.static
+    gcc
+    gdb
+
+  (writeShellApplication {
+      name = "create_disk";
+      runtimeInputs = [
+        pkgs.debootstrap
+      ];
+      text = ''
+      dd if=/dev/zero of=./rootfs.img bs=1G count=2 status=progress
+      mkfs.ext4 ./rootfs.img
+      sudo mount ./rootfs.img /mnt
+      sudo chown -R "$USER:" /mnt
+
+      sudo debootstrap --arch amd64 stable /mnt
+      chroot /mnt /bin/bash<<eof
+      echo -e "pass\npass" | passwd root
+      eof
+      
+      sudo umount /mnt
+      '';
+  })
+
+  (writeShellApplication {
+      name = "start_vm";
+      runtimeInputs = [
+        pkgs.qemu
+      ];
+      text = ''
+      qemu-system-x86_64 \
+          -kernel ./arch/x86_64/boot/bzImage \
+          -append "console=ttyS0,115200 root=/dev/vda rw init=/bin/bash" \
+          -drive file=./rootfs.img,format=raw,if=virtio \
+          -m 2G \
+          -nographic \
+          -enable-kvm
+      '';
+  })
+
   ];
 
   shellHook = ''
